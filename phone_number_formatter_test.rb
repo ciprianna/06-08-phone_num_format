aeca3796f39d46bb478158cfea505bf1fdad5036
Ciprianna <ciprianna17@gmail.com>
2015-06-09 14:49:43 -0500

    Added the test for the format_number method. Test is successful.

--- phone_number_formatter_test.rb
+++ phone_number_formatter_test.rb
@@ -1,7 +1,15 @@
 # Phone Number Formatter Test
-
+require "minitest/autorun"
 require_relative "phone_number_formatter.rb"
 
-class PhoneNumberFormatterTest
+class PhoneNumberFormatterTest < Minitest::Test
+
+  # Need to test the format number method to ensure the inputted String returns
+  #   a properly formatted String to look like a phone number.
+  def test_phone_number_formatter
+    number = PhoneNumber.new("1234567890")
+
+    assert_equal("(123) 456-7890", number.format_number("1234567890"))
+  end
 
 end
