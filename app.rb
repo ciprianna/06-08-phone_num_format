73b377fe05ffb2e6e43d83b535128e96dab57ad1
Ciprianna <ciprianna17@gmail.com>
2015-06-10 11:57:34 -0500

    Refactored app.rb to use active_support. No need for a class.

--- app.rb
+++ app.rb
@@ -1,11 +1,14 @@
 # app.rb
-
-require "./phone_number_formatter.rb"
+require "active_support"
+require "active_support/core_ext/numeric/conversions.rb"
+# require "./phone_number_formatter.rb"
 
 puts "What's your phone number?"
 
-number_input = gets.chomp
+number_input = gets.to_i
 
-test = PhoneNumber.new(number_input)
+puts "That's #{number_input.to_s(:phone, area_code: true)}, right?"
 
-puts "That's #{test.number}, right?"
+# test = PhoneNumber.new(number_input)
+#
+# puts "That's #{test.number}, right?"
